84da7b92794b9dccde6e16a6f251186a4b08872c
Hussey <Husseinaibrahim07@gmail.com>
2024-02-20 23:12:31 +0000

    second commit

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-   function del(){
-       const value=document.getElementById('screen').value;
-       document.getElementById('screen').value=value.substr(0, value.length-1);
-     }
-
-     const one=document.getElementById('one');
-     one.addEventListener('click',function(){
-       console.log('ONE clicked');
-       const body=document.querySelector('body');
-       body.classList.add('active1');
-     })
-     const two=document.getElementById('two');
-     two.addEventListener('click',function(){
-       console.log('TWO clicked');
-       const body=document.querySelector('body');
-       body.classList.remove('active1');
-       body.classList.remove('active2');
-     })
-     const three=document.getElementById('three');
-     three.addEventListener('click',function(){
-       console.log('THREE clicked');
-       const body=document.querySelector('body');
-       body.classList.add('active2');
-       body.classList.remove('active1');
-     })
+function del(){
+  const value=document.getElementById('screen').value;
+  document.getElementById('screen').value=value.substr(0, value.length-1);
+}
+const one=document.getElementById('one');
+one.addEventListener('click',function(){
+  const body=document.querySelector('body');
+  const toggle=document.getElementById('circle');
+  body.classList.remove('active1');
+  body.classList.remove('active2');
+  toggle.style,left='0';
+})
+const two=document.getElementById('two');
+two.addEventListener('click',function(){
+  const body=document.querySelector('body');
+  const toggle=document.getElementById('circle');
+  body.classList.add('active1');
+  body.classList.remove('active2');
+  toggle.style,left='35%';
+})
+const three=document.getElementById('three');
+three.addEventListener('click',function(){
+  const body=document.querySelector('body');
+  const toggle=document.getElementById('circle');
+  body.classList.add('active2');
+  body.classList.remove('active1');
+  toggle.style,left='65%';
+})
